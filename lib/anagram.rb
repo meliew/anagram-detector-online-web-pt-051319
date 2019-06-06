bdd7686e4ac508f60c5afb005ef9015d55b7a936
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-06 18:45:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+def match(words)
+
+words.keep_if do |string| word.split('').sort == string.split('').sort
+
+  end
+end
+
+end
