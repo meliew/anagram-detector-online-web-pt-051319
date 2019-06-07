4b80a69468c332e5c9717f0c7b99694e212c908b
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-07 00:23:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -33,18 +33,18 @@
 
 
 # class Anagram
-# 
+#
 #   attr_accessor :word
-# 
+#
 #   def initialize(word)
 #     @word = word
 #   end
-# 
+#
 # def match(words)
-# 
+#
 # words.keep_if do |string| word.split('').sort == string.split('').sort
-# 
+#
 #   end
 # end
-# 
+#
 # end
